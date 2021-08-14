060b4ce37422d7f7deeca74c83b9983e0e7b43f7
Sinuhé Gómez <sinuhe.gomez@unosquare.com>
2021-08-14 16:07:21 -0500

    testing vue components

---
+++ vueMastery/music/dist/precache-manifest.47a773b11d94baa22869937c0a1166cd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d8c7bce9e47d0946e4a51c7b3e2e80",
+    "url": "/assets/img/header.png"
+  },
+  {
+    "revision": "48025182349af4abfe3e272a7e128c4c",
+    "url": "/assets/img/introduction-music.png"
+  },
+  {
+    "revision": "90840bfca190b45f7cd04ed9d8c2cd54",
+    "url": "/assets/img/introduction-users.png"
+  },
+  {
+    "revision": "eba639c1b0a9287dd3a1904b9ae9831f",
+    "url": "/assets/img/profile-pic.jpg"
+  },
+  {
+    "revision": "d899ba7d2cac3f11c4377a700d68d22e",
+    "url": "/assets/img/song-header.png"
+  },
+  {
+    "revision": "e7b9aeb02c86a922404d13073d21836a",
+    "url": "/assets/img/user-header.png"
+  },
+  {
+    "revision": "a9e4df6ed76a9071658a",
+    "url": "/css/app.341c7812.css"
+  },
+  {
+    "revision": "c08ac0423c7cf1980e15",
+    "url": "/css/chunk-vendors.06a7122d.css"
+  },
+  {
+    "revision": "f485454765c5e41abce3c0f5d9b8cecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e4df6ed76a9071658a",
+    "url": "/js/app.ca4363ae.js"
+  },
+  {
+    "revision": "6e1fcb2df35818d0fd4f",
+    "url": "/js/chunk-2d22d746.fa3cc103.js"
+  },
+  {
+    "revision": "a67f7df9e130a2a6b96a",
+    "url": "/js/chunk-5142c739.78985467.js"
+  },
+  {
+    "revision": "c08ac0423c7cf1980e15",
+    "url": "/js/chunk-vendors.200a00c0.js"
+  },
+  {
+    "revision": "01379305ad78bbde28f8",
+    "url": "/js/groupedChunk.ad5e89c4.js"
+  },
+  {
+    "revision": "f7216665154ef9db8b6fb3462c035e27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
