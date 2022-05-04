509f3f13372e30f08548fa9f0cd7ce87a9146292
sinuheuno <87496412+sinuheuno@users.noreply.github.com>
2022-05-04 18:14:10 -0500

    fix some syles

--- multithreadJS/ch6-mutex/product-mutex.js
+++ multithreadJS/ch6-mutex/product-mutex.js
@@ -1,6 +1,13 @@
-const { i, shared } = workerData;
-const sharedInts = new Int32Array(shared); const mutex = new Mutex(sharedInts, 4); mutex.exec(() => {
-const a = sharedInts[i];
-for (let j = 0; j < 1_000_000; j++) {} const b = sharedInts[3];
-sharedInts[3] = a * b; assert.strictEqual(sharedInts[3], a * b);
+const {
+    i,
+    shared
+} = workerData;
+const sharedInts = new Int32Array(shared);
+const mutex = new Mutex(sharedInts, 4);
+mutex.exec(() => {
+    const a = sharedInts[i];
+    for (let j = 0; j < 1_000_000; j++) {}
+    const b = sharedInts[3];
+    sharedInts[3] = a * b;
+    assert.strictEqual(sharedInts[3], a * b);
 });
